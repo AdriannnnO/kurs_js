d3cbff41ef4c4774b12e7f809579c728cfe29c9e
unknown <okuliczadrian@gmail.com>
2023-10-03 14:26:30 +0200

    jd

---
+++ strings/j.js
@@ -0,0 +1,11 @@
+let firstName = "hujec"
+let lastName = "Pizdziec"
+
+let age = 20
+let dlugoschuja = 30
+
+
+console.log(firstName.toUpperCase()) // upper/lowercase
+console.log(firstName.startsWith('h'))    // true or false
+let result = `this guy ${firstName} ${lastName} has ${age} years `; //connecting variables in a string
+console.log(result)

5c4866b12c56350db98dc25b418741cf00a1d009
unknown <okuliczadrian@gmail.com>
2023-10-05 13:43:49 +0200

    jd

---
+++ arrays/index.js
@@ -0,0 +1,45 @@
+// let items = ['huj','chuj']
+//concat can push multiple elements into an array from other array
+// push = append
+// items.push("hujnia")
+// console.log(items)
+
+//boolean | true or false
+
+t= ['huj','chujnia','jeban']
+// let x = t.includes('huj')
+// console.log(x)
+
+// let hujnia = 1984
+// let hujnia2 = String(hujnia)
+// console.log(typeof hujnia2 + hujnia)
+
+// for (i=0;i<t.length;i++){
+//     console.log(t[i])
+// }
+
+// t_elements = 0
+// while (t_elements<t.length){
+//     console.log(t[t_elements])
+//     t_elements++
+// }
+
+// a="hujniaowinfdoianwhoih"
+
+// if (a=="huj"){
+//     console.log("jd")
+// }else{
+//     if (a=="hujnia"){
+//     console.log("jd2")
+// }
+// else{
+//     console.log("nihuja")
+// }}
+
+// let huj = false
+
+// if (huj){
+//     console.log('ta')
+// }else{
+//     console.log("nie")
+// }
---
+++ breakcontinue/index.js
@@ -0,0 +1,26 @@
+let date = new Date()
+// console.log(date.getDay())
+
+switch(date.getDay()){
+    case 1:
+        console.log("jes poniedzialek");
+        break
+    case 2:
+        console.log("jes wtorek");
+        break
+    case 3:
+        console.log("jes sroda");
+        break
+    case 4:
+        console.log("jes czwartek");
+        break
+    case 5:
+        console.log("jes piontek B)");
+        break
+    case 6:
+        console.log("jes sobota");
+        break
+    case 7:
+        console.log("jes niedziela");
+        break
+}
